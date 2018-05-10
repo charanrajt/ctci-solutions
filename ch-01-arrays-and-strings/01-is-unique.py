65afd98a9ac6f8ab1243f29dea323f1f28400dd5
Charanraj Thimmisetty <charanrajt@gmail.com>
2018-05-10 15:57:14 -0700

    Update 01-is-unique.py

--- ch-01-arrays-and-strings/01-is-unique.py
+++ ch-01-arrays-and-strings/01-is-unique.py
@@ -1,14 +1,28 @@
 # Determine whether or not a given string contains no duplicate characters.
 
-def contains_no_duplicates(string):
-  letters = {}
-  for letter in string:
-    if letter in letters:
-      return False
-    letters[letter] = True
-  return True
+def check_dupl(s):
+    return len(s)!=len(set(s))
+def check_dupl1(s):
+    s1=dict()
+    for i in s:
+        if i in s1:
+            return True
+        else:
+            s1[i]=1
+    return False
+            
+ 
 
-if __name__ == "__main__":
-  import sys
-  print(contains_no_duplicates(sys.argv[-1]))
+from nose.tools import assert_equal
+
+class test(object):
     
+    def test(self,sol):
+        assert_equal(sol('abbc'),True)
+        assert_equal(sol('abc'),False)
+        print "ALL TEST CASES PASSED"
+
+# Run Tests
+t = test()
+t.test(check_dupl)
+t.test(check_dupl1)
