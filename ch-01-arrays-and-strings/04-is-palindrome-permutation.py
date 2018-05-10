fe313fccb60f952425453a841bfd6e4c96c4f26c
Charanraj Thimmisetty <charanrajt@gmail.com>
2018-05-10 16:14:10 -0700

    Update 04-is-palindrome-permutation.py

--- ch-01-arrays-and-strings/04-is-palindrome-permutation.py
+++ ch-01-arrays-and-strings/04-is-palindrome-permutation.py
@@ -1,22 +1,36 @@
-# Determine whether or not a string is a permutation of a palindrome,
+"""
+Determine whether or not a string is a permutation of a palindrome,
 # ignoring spaces.
+"""
 
-def is_palindrome_permutation(string):
-  counter = Counter()
-  for letter in string.replace(" ", ""):
-    counter[letter] += 1
-  #return sum([count % 2 for count in counter.values()]) < 2
-  odd_counts = 0
-  for count in counter.values():
-    odd_counts += count % 2
-    if odd_counts > 1:
-      return False
-  return True
+def func_pp(s):
+    s=s.strip().replace(" ", "")
+    s=s.lower()
+    dic=dict()
+    for i in s:
+        if i in dic:
+            dic[i]+=1
+        else:
+            dic[i]=1
+    odd=0       
+    for key in dic:
+         if dic[key]%2!=0:
+             odd+=1
+         if odd>1:
+             return False
+    return True
+  
 
-class Counter(dict):
-  def __missing__(self, key):
-    return 0
+from nose.tools import assert_equal
 
-if __name__ == "__main__":
-  import sys
-  print(is_palindrome_permutation(sys.argv[-1]))
+class test(object):
+    
+    def test(self,sol):
+        assert_equal(sol('Tact Coa'),True)
+        assert_equal(sol('deval modi'),False)
+
+        print "ALL TEST CASES PASSED"
+
+# Run Tests
+t = test()
+t.test(func_pp)
